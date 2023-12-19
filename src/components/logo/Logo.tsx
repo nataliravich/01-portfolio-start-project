ef08598874036f727692f67ad03d6cb62350b986
nataliravich <ravich.natali@gmail.com>
2023-12-19 19:00:51 +0200

    create Main

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Icon } from "../../assets/icon/Icon";
+import { Icon } from "../icon/Icon";
 
 
 export const Logo = () => {
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import photo from "../../../assets/images/photo.jpg"
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+	return (
+		<StyledMain >
+			<FlexWrapper align={"center"} justify={"space-around"}>
+				<div>
+					<span>Hi There</span>
+					<Name>I am Svetlana Dyablo</Name>
+					<MainTitle>A Web Developer.</MainTitle>
+				</div>
+				<Photo src={photo} alt="" />
+			</FlexWrapper>
+		</StyledMain >
+	);
+};
+
+const StyledMain = styled.div`
+min-height: 100vh;
+background-color: #c2c491;
+`
+
+const Photo = styled.img`
+	width: 350px;
+	height: 430px;
+	object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+
+`
+
+const Name = styled.h2`
+
+`
