c5dd07a8c900ee0ae433f6b1e2f1c075d6c4f2c4
nataliravich <ravich.natali@gmail.com>
2023-12-21 23:37:56 +0200

    create Works

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Header } from './layout/header/Header';
 import { Main } from './layout/sections/main/Main';
 import { Skills } from './layout/sections/skills/Skills';
+import { Works } from './layout/sections/works/Works';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Header />
 			<Main />
 			<Skills />
+			<Works />
 		</div>
 	);
 }
