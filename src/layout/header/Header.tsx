c7c3c4ff3b942314e302b76c2e7409c0abd3e863
nataliravich <ravich.natali@gmail.com>
2024-01-10 10:24:46 +0200

    add fonts, global styles, defined & set the main colors, created a container

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
+import { Container } from "../../components/Container";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
 
 export const Header = () => {
 	return (
 		<StyledHeader>
-			<Logo />
-			<Menu menuItems={items} />
+			<Container>
+				<FlexWrapper justify={"space-between"} align={"center"}>
+					<Logo />
+					<Menu menuItems={items} />
+				</FlexWrapper>
+			</Container>
 		</StyledHeader>
 	);
 };
 
 const StyledHeader = styled.header`
-	background-color: #d4ffd3;
-	display: flex;
-	justify-content: space-between;
+	background: rgba(31, 31, 32, 0.90);
 `
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -18,7 +18,7 @@ export const Main = () => {
 	);
 };
 
-const StyledMain = styled.div`
+const StyledMain = styled.section`
 min-height: 100vh;
 background-color: #c2c491;
 `
