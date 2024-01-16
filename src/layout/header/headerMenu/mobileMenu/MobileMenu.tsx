236dadbc4dbcc4942139cccac740de21fa8a781a
nataliravich <ravich.natali@gmail.com>
2024-01-16 15:28:37 +0200

    Burger menu (useState)

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "../menu/Menu";
 import { S } from "../HeaderMenu_Styles";
 
 
 export const MobileMenu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
+	const [menuIsOpen, setmenuIsOpen] = useState(false);
+	const onBurgerBtnClick = () => { setmenuIsOpen(!menuIsOpen) }				/** ! - менеяет значение переменной на противоположное*/
 	return (
 		<S.MobileMenu>
-			<S.BurgerButton isOpen={false}>					{/**задали атрибут для нажатия*/}
+			<S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>	{/**задали атрибут для нажатия, передаем,что в переменной: false или true*/}
 				<span></span>
 			</S.BurgerButton>
-			<S.MobileMenuPopup isOpen={false}>				{/**задали атрибут для нажатия*/}
+
+			<S.MobileMenuPopup isOpen={menuIsOpen} onClick={() => { setmenuIsOpen(false) }}>		{/**закрывает меню при нажатии в любом месте*/}
 				<Menu menuItems={props.menuItems} />
 			</S.MobileMenuPopup>
 		</S.MobileMenu >
